18b2cfdd1366fed83459fd1d9fc4cee1736d8e17
Sabr <sabo98@mail.ru>
2025-11-28 18:30:22 +0500

    страница регистрации готова

--- src/main/java/org/example/controller/SignUpController.java
+++ src/main/java/org/example/controller/SignUpController.java
@@ -2,11 +2,17 @@ package org.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SignUpController {
     @GetMapping("/sign-up")
-    public String signUp(){
+    public String getSignUpPage(){
+        return "sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String postSignUpPage(){
         return "sign-up";
     }
 }
