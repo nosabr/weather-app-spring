ac98d971a5a0ed062c448f7a66f8986a321023d6
Sabr <sabo98@mail.ru>
2025-12-03 16:19:50 +0500

    поменял погода дто

--- src/main/java/org/example/dto/WeatherResponseDTO.java
+++ src/main/java/org/example/dto/WeatherResponseDTO.java
@@ -1,23 +1,19 @@
 package org.example.dto;
 
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.example.dto.weather.Coord;
-import org.example.dto.weather.Main;
-import org.example.dto.weather.Weather;
-import org.example.dto.weather.Wind;
-
-import java.util.List;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class WeatherResponseDTO {
-    private Coord coord;
-    private List<Weather> weather;
-    private Main main;
-    private Wind wind;
-    private String name;
+    private String name;           // Название города
+    private String country;        // Код страны
+    private Integer temperature;   // Температура
+    private Integer feelsLike;     // Ощущается как
+    private Integer humidity;      // Влажность
+    private String description;    // Описание погоды
+    private String icon;           // Иконка погоды
 }
